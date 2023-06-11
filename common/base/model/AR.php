7552768e40bb5c41f76bc15585a9b4091b83eda2
supermetrolog <billypro6@gmail.com>
2023-06-11 17:02:02 +0300

    developed soft delete feature

--- common/base/model/AR.php
+++ common/base/model/AR.php
@@ -3,10 +3,18 @@
 namespace common\base\model;
 
 use common\base\exception\ValidateException;
+use DateTime;
+use Throwable;
+use yii\base\ErrorException;
 use yii\db\ActiveRecord;
+use yii\db\StaleObjectException;
 
 class AR extends ActiveRecord
 {
+    public const SOFT_DELETE_ATTRIBUTE = 'deleted_at';
+
+    protected bool $useSoftDelete = true;
+
     /**
      * @param bool $validate
      * @return void
@@ -17,5 +25,36 @@ class AR extends ActiveRecord
         if (!$this->save($validate)) {
             throw new ValidateException($this);
         }
+    }
+
+    /**
+     * @return void
+     * @throws Throwable
+     * @throws StaleObjectException
+     */
+    public function delete(): void
+    {
+        if ($this->useSoftDelete) {
+            $this->softDelete();
+        } else {
+            if (!parent::delete()) {
+                throw new ErrorException('Delete model error');
+            }
+        }
+    }
+
+    /**
+     * @return void
+     * @throws ErrorException
+     * @throws ValidateException
+     */
+    protected function softDelete(): void
+    {
+        if (!$this->hasAttribute(self::SOFT_DELETE_ATTRIBUTE)) {
+            throw new ErrorException('Soft delete attribute (' . self::SOFT_DELETE_ATTRIBUTE . ') not exist');
+        }
+
+        $this->setAttribute(self::SOFT_DELETE_ATTRIBUTE, (new DateTime())->format('Y-m-d H:i:s'));
+        $this->saveOrThrow();
     }
 }
