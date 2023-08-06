e355f5c0f164b38fd3c4af350029af9d8b1dcfbc
supermetrolog <billypro6@gmail.com>
2023-08-06 16:56:10 +0300

    change docker config

--- init.sh
+++ init.sh
@@ -3,21 +3,19 @@ chmod 777 -R ./backend/runtime
 
 chmod 777 -R ./backend/web/assets
 
-chmod 777 -R ./backend/web/assets
-
-chmod 777 -R ./backend/tests/_output
+#chmod 777 -R ./backend/tests/_output
 
 chmod 777 -R ./frontend/runtime
 
 chmod 777 -R ./frontend/web/assets
 
-chmod 777 -R ./frontend/web/assets
+chmod 777 -R ./console/runtime
 
-chmod 777 -R ./frontend/tests/_output
+#chmod 777 -R ./frontend/tests/_output
 
 
-chmod 777 -R ./common/tests/_output
-chmod 777 -R ./console/migrations
-chmod 666 ./composer.json
-chmod 666 ./composer.lock
-chmod 777 -R ./vendor
+#chmod 777 -R ./common/tests/_output
+#chmod 777 -R ./console/migrations
+#chmod 666 ./composer.json
+#chmod 666 ./composer.lock
+#chmod 777 -R ./vendor
